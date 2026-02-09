01cbd91a1b8afb1e964871e53217d36d5b721537
Joel Joseph <joel.biju.joseph@gmail.com>
2026-02-08 18:56:01 -0600

    publishing to pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(() => {
+  const repo = process.env.GITHUB_REPOSITORY?.split('/')?.[1]
+  const base = repo ? `/${repo}/` : '/'
+
+  return {
+    plugins: [react()],
+    base,
+  }
 })
